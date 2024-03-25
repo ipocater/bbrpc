7f5d8259fb5ae51c565839700e7c5705d4023aa8
wh_xubang <v-xubang1.gd@chinatelecom.cn>
2024-03-25 17:33:33 +0800

    refactor to http invoc

--- bbrpc-core/src/main/java/com/ipoca/bbrpc/core/consumer/ConsumerBootstrap.java
+++ bbrpc-core/src/main/java/com/ipoca/bbrpc/core/consumer/ConsumerBootstrap.java
@@ -5,11 +5,8 @@ import com.ipoca.bbrpc.core.api.LoadBalancer;
 import com.ipoca.bbrpc.core.api.RegistryCenter;
 import com.ipoca.bbrpc.core.api.Router;
 import com.ipoca.bbrpc.core.api.RpcContext;
-import com.ipoca.bbrpc.core.registry.ChangedListener;
-import com.ipoca.bbrpc.core.registry.Event;
 import com.ipoca.bbrpc.core.util.MethodUtils;
 import lombok.Data;
-import org.apache.logging.log4j.util.Strings;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.EnvironmentAware;
@@ -17,13 +14,10 @@ import org.springframework.core.env.Environment;
 
 import java.lang.reflect.Field;
 import java.lang.reflect.Proxy;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
-
-import static com.ipoca.bbrpc.core.util.MethodUtils.findAnnotatedField;
 
 /**
  *@Author：xubang
@@ -60,13 +54,13 @@ public class ConsumerBootstrap  implements ApplicationContextAware, EnvironmentA
                     Object consumer = stub.get(serviceName);
                     if (consumer == null){
                         consumer = createFromRegisry(service, context, rc);
+                        stub.put(serviceName, consumer);
                     }
                     f.setAccessible(true);
                     f.set(bean, consumer);
                 } catch (Exception ex){
                     ex.printStackTrace();
                 }
-
             });
         }
     }
@@ -92,9 +86,6 @@ public class ConsumerBootstrap  implements ApplicationContextAware, EnvironmentA
     private Object createConsumer(Class<?> service, RpcContext context, List<String> providers) {
         return Proxy.newProxyInstance(service.getClassLoader(),
                 new Class[]{service}, new BBInvocationHandler(service, context, providers));
-
     }
-
-
 
 }
