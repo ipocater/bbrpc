13e4fc8e99614b0c91c4cb0c76adb4e130d055de
wh_xubang <v-xubang1.gd@chinatelecom.cn>
2024-03-29 17:10:29 +0800

    pom change commit

--- bbrpc-demo-consumer/src/test/java/com/ipoca/bbrpc/demo/consumer/BbrpcDemoConsumerApplicationTests.java
+++ bbrpc-demo-consumer/src/test/java/com/ipoca/bbrpc/demo/consumer/BbrpcDemoConsumerApplicationTests.java
@@ -1,13 +1,30 @@
 package com.ipoca.bbrpc.demo.consumer;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootTest
 class BbrpcDemoConsumerApplicationTests {
 
+    static ApplicationContext context;
+
+    @BeforeAll
+    static void init(){
+        //context = SpringApplication.run()
+    }
+
     @Test
     void contextLoads() {
+        System.out.println(" ===> aaaa .....");
+    }
+
+    @AfterAll
+    static void destory(){
+        SpringApplication.exit(context, () -> 1);
     }
 
 }
