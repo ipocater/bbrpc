c495194bb73fc5e89d9f84a06887df4fea8b67d7
ipocater <il>
2024-04-08 23:53:01 +0800

    add param to instance

--- bbrpc-core/src/main/java/com/ipoca/bbrpc/core/meta/ServiceMeta.java
+++ bbrpc-core/src/main/java/com/ipoca/bbrpc/core/meta/ServiceMeta.java
@@ -1,7 +1,11 @@
 package com.ipoca.bbrpc.core.meta;
 
+import com.alibaba.fastjson.JSON;
 import lombok.Builder;
 import lombok.Data;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * 描述provider的映射关系
@@ -18,7 +22,12 @@ public class ServiceMeta {
     private String env;
     private String name;
 
+    private Map<String, String> parameters = new HashMap<>();
     public String toPath() {
         return String.format("%s_%s_%s_%s", app, namespace, env, name);
+    }
+
+    public String toMetas() {
+        return JSON.toJSONString(this.getParameters());
     }
 }
